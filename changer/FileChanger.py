56276265f627c03ec0152744ee9c6b4850f02c3f
Mikhail Fesenko <proggga@gmail.com>
2017-04-06 14:57:48 +0300

    fix many changes

--- changer/FileChanger.py
+++ changer/FileChanger.py
@@ -14,6 +14,7 @@ class FileChanger(object):
         self.config_path = config_path
         self.file = path_to_file
         self.host_address = None
+        self.host_port = None
         self.hostname = None
         self.file_content = None
         self.load_config()
@@ -41,10 +42,12 @@ class FileChanger(object):
 
     def parse_file_content(self):
         for line in self.file_content.split('\n'):
-            valid_match = self.valid(line)
-            if valid_match:
-                self.host_address = valid_match.group(1)
-                break
+            valid_match_host = self.valid_host(line)
+            valid_match_port = self.valid_port(line)
+            if valid_match_host:
+                self.host_address = valid_match_host.group(1)
+            elif valid_match_port:
+                self.host_port = valid_match_port.group(1)
         for hostname, hostdata in self.hosts.items():
             if self.host_address == hostdata['address']:
                 self.hostname = hostname
@@ -58,12 +61,19 @@ class FileChanger(object):
             logging.info("config have no '{}' record, replacing by first record in config".format(self.host_address))
             self.switch_to_next_host()
 
-    def valid(self, line):
+    def valid_host(self, line):
         return re.match(r'^(?!#)(?:\s*)host(?:\s*)=(?:\s*)([A-Za-z0-9\._]*)', line.strip())
+
+    def valid_port(self, line):
+        return re.match(r'^(?!#)(?:\s*)port(?:\s*)=(?:\s*)([0-9\._]*)', line.strip())
 
     def valid_substring(self, line):
         return re.sub(r'^(?!#)(?:\s*)host(?:\s*)=(?:\s*)([A-Za-z0-9\._]*)',\
             'host = {}'.format(self.host_address), line.strip())
+
+    def valid_substring_port(self, line):
+        return re.sub(r'^(?!#)(?:\s*)port(?:\s*)=(?:\s*)([0-9]*)',\
+            'port = {}'.format(self.host_port), line.strip())
 
     def switch_to_next_host(self):
         server_found = False
@@ -86,10 +96,16 @@ class FileChanger(object):
         server_name, data = new_server
         self.hostname = server_name
         self.set_host_ip(data['address'])
+        self.set_host_port(data['port'])
 
     def set_host_ip(self, address):
         logging.info("Host {} changed to {}".format(self.host_address, address))
         self.host_address = address
+        self.replace()
+
+    def set_host_port(self, port):
+        logging.info("Port {} changed to {}".format(self.host_port, port))
+        self.host_port = port
         self.replace()
 
     def search_and_replace(self, hostname=''):
@@ -101,6 +117,7 @@ class FileChanger(object):
             self.hosts[hostname].checked = True
             self.hostname = hostname
             self.set_host_ip(self.hosts[hostname]['address'])
+            self.set_host_port(self.hosts[hostname]['port'])
         elif is_ip:
             ipaddress = is_ip.group(1)
             logging.info('this ip {} is valid'.format(ipaddress))
@@ -124,6 +141,8 @@ class FileChanger(object):
             file_content = file_handler.read()
             file_handler.seek(0)
             for line in file_content.split('\n'):
-                file_handler.write(self.valid_substring(line)+self.appendNewLine(line))
+                newline = self.valid_substring(line)
+                newline = self.valid_substring_port(newline)
+                file_handler.write(newline + self.appendNewLine(line))
             file_handler.truncate()
             file_handler.close()
